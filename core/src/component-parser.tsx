b6e96c00bbab44ddac57b06164aaf3927423131a
mai.shu <ansiboy@163.com>
2020-02-29 08:40:02 +0800

    创建 component parser 和 inner components

---
+++ core/src/component-parser.tsx
@@ -0,0 +1,62 @@
+
+import { ComponentData } from "./component-data";
+import React = require("react");
+import { View } from "@tarojs/components";
+
+type compoenntTypes = { [name: string]: typeof React.Component|React.ComponentType};
+
+
+let errors = {
+    pathFieldRequired(name: string) {
+        let msg = `Path field of '${name}' component config can not be null or empty.`;
+        return new Error(msg);
+    },
+    canntFindModule(name: string, path: string) {
+        let msg = `Can not find component '${name}' in the module, module path is: '${path}'.`;
+        return new Error(msg);
+    },
+    componentNotExists(name: string) {
+        let msg = `Component '${name}' not exists.`;
+        return new Error(msg);
+    }
+}
+
+export class CompoenntParser {
+    private compoenntTypes: compoenntTypes;
+
+    constructor(compoenntTypes: compoenntTypes) {
+        this.compoenntTypes = compoenntTypes;
+    }
+
+    parse(componentData: ComponentData) {
+        try {
+            return this.createComponent(componentData);
+        }
+        catch (e) {
+            let err: Error = e;
+            return <View>
+                <View>Error:</View>
+                <View>{err.message}</View>
+            </View>
+        }
+    }
+
+    private createComponent(componentData: ComponentData | string) {
+
+        if (typeof componentData == "string")
+            return componentData;
+
+        let children = (componentData.children || []).map(c => this.createComponent(c));
+
+        let props = (componentData.props || {}) as any;
+        let name = componentData.type;
+        let compoenntType = this.compoenntTypes[name];
+
+        if (compoenntType == null) {
+            throw errors.componentNotExists(name);
+        }
+
+        let r = React.createElement(compoenntType, props, children);
+        return r;
+    }
+}
--- core/src/index.ts
+++ core/src/index.ts
@@ -1,4 +1,5 @@
-export * from "./component-data";
-export * from "./component-info";
-export * from "./decorators";
-export * from "./page-view";
+export { ComponentData } from "./component-data";
+export { ComponentInfo } from "./component-info";
+export { component } from "./decorators";
+export { PageView } from "./page-view";
+export { taroComponents, tarouiComponents } from "./inner-components";
