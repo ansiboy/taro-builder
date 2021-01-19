867bc34e4cd90b7c88a2821622020fda69b9cf28
mai.shu <ansiboy@163.com>
2021-01-19 10:09:34 +0800

    改字段及同步

--- config.js
+++ config.js
@@ -4,9 +4,10 @@ const m = require("maishu-node-data");
 /** @type m.ConnectionOptions */
 let db = {
     type: "mysql", host: "127.0.0.1", username: "root", password: "81263",
-    port: 3306, name: "taro-builder", database: "taro-builder", entities: ["./out/entities.js"]
+    port: 3306, name: "taro-builder", database: "taro-builder", entities: ["./out/entities.js"],
+    synchronize: false,
 };
-let componentStation = "http://127.0.0.1:6736";
+let componentStation = "http://192.168.2.118:6736";
 let port = 5216;
 //=============================================================
 
