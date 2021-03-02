690be93986080ea082ac7d0565c951ebf902d157
mai.shu <ansiboy@163.com>
2021-03-02 11:35:38 +0800

    update

--- src/static/init.ts
+++ src/static/init.ts
@@ -1,26 +1,27 @@
 window["h5"] = "weapp";
 
-import { InitArguments } from "maishu-chitu-admin/static";
+// import { InitArguments } from "maishu-chitu-admin/static";
 import { Less } from "maishu-ui-toolkit";
 import * as React from "react";
 import "content/style.scss";
 import "site.less";
 import { pathConcat } from "maishu-toolkit";
+import { Application } from "maishu-admin-scaffold/static/application";
 
 window["react"] = React;
-export default async function (args: InitArguments) {
+export default async function (app: Application) {
 
-    Less.renderByRequireJS("devices",);
+    // Less.renderByRequireJS("devices",);
 
-    let loadjs = args.app["loadjs"];
-    args.app["loadjs"] = function (path) {
-        if (path.startsWith("modules//")) {
-            let pagePath = path.substr("modules//".length);
-            let arr = pagePath.split("/");
-            let stationPath = arr.shift();
-            path = pathConcat(stationPath, "modules", arr.join("/"));
+    // let loadjs = args.app["loadjs"];
+    // args.app["loadjs"] = function (path) {
+    //     if (path.startsWith("modules//")) {
+    //         let pagePath = path.substr("modules//".length);
+    //         let arr = pagePath.split("/");
+    //         let stationPath = arr.shift();
+    //         path = pathConcat(stationPath, "modules", arr.join("/"));
 
-        }
-        return loadjs.apply(this, [path]);
-    }
+    //     }
+    //     return loadjs.apply(this, [path]);
+    // }
 }
